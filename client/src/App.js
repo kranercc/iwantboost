579a1322fab62d65953fb8c0c7fc752faef24a7d
kranercc <kranehecaru@gmail.com>
2022-01-05 08:21:47 +0200

    jaijaigaming

--- client/src/App.js
+++ client/src/App.js
@@ -108,7 +108,7 @@ function App() {
 
                 <div className='main-content'>
                     <BoosterCard email="kranedota3@gmail.com" discord="krane#2890" id="krane" mmr="5600 MMR" img="https://www.earlygame.com/uploads/images/meepo-guide-fan-art.jpg" booster_name="krane" booster_description="I have played dota2 for a long while, and i gathered the information needed to boost you to the next level"></BoosterCard>
-                    <BoosterCard id="Become a booster" mmr="0 MMR" booster_name="Become a booster" booster_description="First 3 boosters have their place for free for 2 weeks" img="https://besthqwallpapers.com/Uploads/25-3-2019/84664/thumb2-broodmother-monster-dota-2-spiders-artwork.jpg"></BoosterCard>
+                    <BoosterCard email="jais4noreturn@gmail.com" discord="pixzy#2798" id="Jaijaigaming" mmr="5790 MMR" booster_name="Jaijaigaming" booster_description="I can play dota 24/7, I have played since 2014 and i'm ready to boost you to the next level. Contact me, I respond fast ;)" img="https://cdn.discordapp.com/attachments/928166531119054938/928168894319976469/FB_IMG_1639647084554.jpg"></BoosterCard>
                     <BoosterCard id="Become a booster" mmr="0 MMR" img="https://i.pinimg.com/originals/33/a8/5a/33a85aae18c8c22d9d36ef8fc8fd7759.jpg" booster_name="Become a booster" booster_description="First 3 boosters have their place for free for 2 weeks"></BoosterCard>
                     <BoosterCard id="Become a booster" mmr="0 MMR" img="https://dmarket.com/blog/best-dota2-wallpapers/dota-2-wallpapers-invoker-dark-artsitry_hu2b9f37a952268d90694843d83f8c4f13_444148_1920x1080_resize_q75_lanczos.jpg" booster_name="Become a booster" booster_description="First 3 boosters have their place for free for 2 weeks"></BoosterCard>
                 </div>
@@ -121,3 +121,4 @@ function App() {
 }
 
 export default App;
+
