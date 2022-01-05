7f548a038eb39f592d09c63b6c07f771a4b1c5c3
kranercc <kranehecaru@gmail.com>
2022-01-05 06:39:21 +0200

    SSL, client/server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
+
+const hostname = 'localhost'
+const port = 3443
+const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, "./ssl/sslkey.key")),
+    cert: fs.readFileSync(path.join(__dirname, "./ssl/iwantboost_com.crt")),
+    ca: fs.readFileSync(path.join(__dirname, "./ssl/iwantboost_com.ca-bundle"))
+}
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))  
+})
+
+https.createServer(httpsOptions, app).listen(port, hostname, function() {
+  console.log(`Server running at https://${hostname}:${port}/`)
+})
